12d33d08163f127f1803ff06cf58d132effaea66
wanghaoxi3000 <wanghaoxi3000@163.com>
2023-08-20 17:08:44 +0800

    Init  notion backup branch

---
+++ elog.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  write: {
+    platform: 'notion',
+    notion: {
+      token: process.env.NOTION_TOKEN,
+      databaseId: process.env.NOTION_DATABASE_ID,
+      limit: 1,
+      filter: {
+        and: [
+          {
+            property: 'status',
+            select: { equals: 'Published' }
+          },
+          {
+            property: 'type',
+            select: { equals: 'Post' }
+          },
+        ]
+      },
+      sorts: false, // [{timestamp: 'created_time', direction: 'descending'}],
+      catalog:  {
+        enable: true,
+        property: "date",
+      },
+    },
+  },
+  deploy: {
+    platform: 'local',
+    local: {
+      outputDir: './article',
+      filename: 'title',
+      format: 'matter-markdown',
+      catalog: true,
+      formatExt: './formatext.js',
+    },
+  },
+  image: {
+    enable: true,
+    platform: 'local',
+    local: {
+      outputDir: './images',
+      prefixKey: '../../images',
+    }
+  },
+}
---
+++ formatext.js
@@ -0,0 +1,24 @@
+const { matterMarkdownAdapter } = require("@elog/plugin-adapter");
+
+/**
+ * 自定义文档处理器
+ * @param {DocDetail} doc doc的类型定义为 DocDetail
+ * @return {string} 返回处理后的文档内容字符串
+ */
+const format = (doc) => {
+  if (doc.properties) {
+    let properties = doc.properties
+
+    for (var key in properties) {
+      if (properties.hasOwnProperty(key) && properties[key] === "") {
+        delete properties[key];
+      }
+    }
+  }
+
+  return matterMarkdownAdapter(doc);
+};
+
+module.exports = {
+  format,
+};
